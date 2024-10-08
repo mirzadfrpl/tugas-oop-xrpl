30a46566b7c8b92a0ff25eeb09effc33eb315c9a
mirzadfrpl <mirzadf123@gmail.com>
2024-10-08 11:02:50 +0700

    new

--- oopMirza.php
+++ oopMirza.php
@@ -4,14 +4,16 @@ class Employed {
 
     public $nama;
     public $salary;
+    public $posisi;
 
-    public function __construct($nama, $salary)
+    public function __construct($nama, $salary, $posisi)
     {
         $this->nama = $nama;
         $this->salary = $salary;
+        $this->salary = $posisi;
     }
 }
 
-$employed1 = new Employed("Mirza", 5000);
+$employed1 = new Employed("Mirza", 5000, true);
 
 var_dump($employed1);
